8848a9505c03e4e070029fabe1a59cb61162af61
Abdullateef Suleiman <abdullateef@macs-MacBook-Pro.local>
2021-10-14 13:56:10 +0100

    bug fixex

--- src/conrollers.js
+++ src/conrollers.js
@@ -4,8 +4,9 @@ const validUrl = require("valid-url");
 const URL = process.env.PROD_URL;
 
 const createUrl = async (req, res) => {
-  const url = req.body.url;
-  res.send(url);
+  res.send("hello from create url");
+  //   const url = req.body.url;
+  //   res.send(url);
   //   const isValid = validUrl.isUri(url);
   //   if (!isValid) {
   //     throw new Error("Invalid Url");
