8ac6aea38af7393365b468535a3904d18d9f07e5
Abdullateef Suleiman <abdullateef@macs-MacBook-Pro.local>
2021-10-14 13:34:37 +0100

    updated url

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { createUrl, redirect } = require("./conrollers");
 
 router.get("/", (req, res) => res.send("We are live on heroku !"));
-router.post("/", createUrl);
+router.post("/url", createUrl);
 router.get("/:code", redirect);
 
 module.exports = router;
