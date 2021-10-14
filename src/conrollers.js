120a0f3dd25c7b7ef464ec7cea9ae70587738927
Abdullateef Suleiman <abdullateef@macs-MacBook-Pro.local>
2021-10-14 13:38:01 +0100

    updated url

--- src/conrollers.js
+++ src/conrollers.js
@@ -6,31 +6,31 @@ const URL = process.env.PROD_URL;
 const createUrl = async (req, res) => {
   const url = req.body.url;
   console.log(url);
-  const isValid = validUrl.isUri(url);
-  if (!isValid) {
-    throw new Error("Invalid Url");
-  }
-  const isExist = await UrlModel.findOne({ long_url: url });
-  //   console.log(isExist);
-  if (isExist) {
-    return res.status(200).json({
-      message: "Available",
-      short_url: isExist.short_url,
-    });
-  }
-  const shortCode = shortId.generate();
-  const shortUrl = URL + "/" + shortCode;
-  console.log(shortUrl);
-  const newUrl = new UrlModel({
-    long_url: url,
-    short_url: shortUrl,
-    url_code: shortCode,
-  });
-  await newUrl.save();
-  return res.json({ short_url: shortUrl });
+  //   const isValid = validUrl.isUri(url);
+  //   if (!isValid) {
+  //     throw new Error("Invalid Url");
+  //   }
+  //   const isExist = await UrlModel.findOne({ long_url: url });
+  //   //   console.log(isExist);
+  //   if (isExist) {
+  //     return res.status(200).json({
+  //       message: "Available",
+  //       short_url: isExist.short_url,
+  //     });
+  //   }
+  //   const shortCode = shortId.generate();
+  //   const shortUrl = URL + "/" + shortCode;
+  //   console.log(shortUrl);
+  //   const newUrl = new UrlModel({
+  //     long_url: url,
+  //     short_url: shortUrl,
+  //     url_code: shortCode,
+  //   });
+  //   await newUrl.save();
+  //   return res.json({ short_url: shortUrl });
 };
 const redirect = async (req, res) => {
-    const code = req.params.code;
+  const code = req.params.code;
   const destination = await UrlModel.findOne({ url_code: code });
   if (!destination) {
     return res.status(400).json({
