20b57258ea2d95dbc1e90ef87d76abe700a345ac
Abdullateef Suleiman <abdullateef@macs-MacBook-Pro.local>
2021-10-14 12:55:23 +0100

    added cors

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 4500;
 const connectDB = require("./src/db");
+const cors = require("cors");
 const urlrouter = require("./src/routes");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 connectDB();
 
 app.use(urlrouter);
